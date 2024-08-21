96d8819c435346407aeac3c797246725bf1ec9cb
Dennis Smith <denniskofismith@gmail.com>
2024-08-21 16:02:40 +0000

    Assignment

--- Week_1/Day_3/User_Inputs.py
+++ Week_1/Day_3/User_Inputs.py
@@ -6,9 +6,32 @@ y = "years" if int(age) > 1 else "year"
 
 print ("Hello " + name + "! You are " + age + " " + y + " old.")
 
-if 0 < int(age)  < 24:
+if age <= 0 :
+       print("Age is less than or equal to  0")
+
+elif 0 < int(age)  < 24:
+
     print( "hello" + name + "you are " + age + "years" + "keep playing")
+
 elif 13 >= int(age) < 24:
+
     print("hello" + name + "you are " + age + "years" + "start looking for job")
+
 else:
     print("hello" + name + "you are " + age + "years" + "stop playing and start thinking")
+
+
+
+
+### ASSIGNMENTS
+#   fuctions
+#   modules
+#   web Api's
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
