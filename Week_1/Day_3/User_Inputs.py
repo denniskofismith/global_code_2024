d535a4dfe7ee272aa928157452296c0db129a16f
ansah britwum kwabena <britwumansah@gmail.com>
2024-08-21 15:41:11 +0000

    Kaywuo

--- Week_1/Day_3/User_Inputs.py
+++ Week_1/Day_3/User_Inputs.py
@@ -1,7 +1,14 @@
 name = input("What is your name? ")
 
-age = input("What is your Age? ")
+age = (input("What is your Age? "))
 
 y = "years" if int(age) > 1 else "year"
 
 print ("Hello " + name + "! You are " + age + " " + y + " old.")
+
+if 0 < int(age)  < 24:
+    print( "hello" + name + "you are " + age + "years" + "keep playing")
+elif 13 >= int(age) < 24:
+    print("hello" + name + "you are " + age + "years" + "start looking for job")
+else:
+    print("hello" + name + "you are " + age + "years" + "stop playing and start thinking")
