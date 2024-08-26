da63d100226b022357562d5636b6cc864c6841a7
Dennis Smith <denniskofismith@gmail.com>
2024-08-26 16:44:06 +0000

    Advance Python Global Code Week1 Day 1

--- week_2/day_1/Challenge:MP3Class.py
+++ week_2/day_1/Challenge:MP3Class.py
@@ -1,13 +1,22 @@
 class mp3playlist:
+    tracks = []
+    def __init__(self,tracks) :
+        self.tracks = tracks
 
 
-    def playlist(slef):
-        f = open("tracks.txt", "r")
-        print( f.readline())
 
+    def playlist(self):
+
+        self.display_tracks()
+
+    def display_tracks(self):
+
+        for t in self.display_tracks:
+            print(t)       
+
+    def add_playlist(self,playlist):
+        
+        playlist.append()
+        
     
 
-
-koddy = mp3playlist()
-
-print(koddy.playlist())
