af7cd867c39af8a5d8b7fd291e881bbf312ee38f
Dennis Smith <denniskofismith@gmail.com>
2024-08-28 11:41:12 +0000

    Web Application with Flask

---
+++ week_2/day_3/webapp.py
@@ -0,0 +1,22 @@
+from flask import Flask , render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    #return '<p>Hello World!</p> <p> 17th May </p> <p>Watch Chelsea Play</p>'
+    return render_template('index.html')
+
+@app.route('/whereami')
+def whereami():
+    return 'Ghana!'
+
+@app.route('/foo/<name>')
+
+def foo(name):
+    return render_template('index2.html', to= name)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
